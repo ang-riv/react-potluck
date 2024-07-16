d4c68cc3894b61b4bb06a9fb74ebe385ac7cc495
a.rivera <a.riveraa99@gmail.com>
2024-07-16 16:44:00 -0500

    Added modal to show if the user has not chosen their number of guests

--- src/Button.js
+++ src/Button.js
@@ -8,20 +8,38 @@ function MyButton(props) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const test = console.log("At zero!");
+  const div = props.invitedRef;
+  const input = props.inputRef;
+  //const test = console.log("At zero!");
 
-  // event handler for when the button is pressed
+  // event handler for when the button is pressed and guestNum is not empty
+  function handleClick() {}
   // if the number of guests is zero
   if (props.num === 0) {
     return (
       <>
         <Button onClick={handleShow}>Submit Guests</Button>
-        {test}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Invalid Entry</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Please chose the number of guests that are attending.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              OK
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </>
     );
   } else {
-    return <Button variant="warning">Submit Guests</Button>;
+    return (
+      <Button variant="warning" onClick={handleClick}>
+        Submit Guests
+      </Button>
+    );
   }
 }
-
 export default MyButton;
