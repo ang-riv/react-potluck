1e6ea09e726f93b1c1e06d4b82ffec0de2ac2446
a.rivera <a.riveraa99@gmail.com>
2024-07-23 11:16:35 -0500

    Removed confetti and hid assign dishes btn, show try again button after clicked

--- src/AssignDishes.js
+++ src/AssignDishes.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import MyButton from "./Button";
-import ConfettiExplosion from "react-confetti-explosion";
+
 function AssignDishes(props) {
-  // TODO: add a try again button and hide the assign dishes button after it is clicked, and add confetti
   return (
     <div ref={props.div}>
       {/*testing*/}
@@ -20,7 +18,6 @@ function AssignDishes(props) {
         <MyButton innerText="Assign Dishes" onClick={props.onClick} />
       ) : (
         <>
-          <Button disabled>Assign Dishes</Button>
           <Button>Try Again?</Button>
         </>
       )}
